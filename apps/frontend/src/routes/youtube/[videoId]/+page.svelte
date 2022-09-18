43fd12c2607d8689f321eee3e60dc5f66fd3819b
jangoergens <22739836+jangoergens@users.noreply.github.com>
2022-09-18 17:02:44 +0200

    Refactor route parameter access

--- apps/frontend/src/routes/youtube/[videoId]/+page.svelte
+++ apps/frontend/src/routes/youtube/[videoId]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import type { RouteParams } from '.svelte-kit/types/src/routes/youtube/[videoId]/$types';
+	import { page } from '$app/stores';
 
-	export let data: RouteParams;
+	const videoId = $page.params.videoId;
 </script>
 
 <div>
 	<iframe
 		width="560"
 		height="315"
-		src={'https://www.youtube.com/embed/' + data.videoId}
+		src={'https://www.youtube.com/embed/' + videoId}
 		title="YouTube video player"
 		frameborder="0"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
--- apps/frontend/src/routes/youtube/[videoId]/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { Page } from '@sveltejs/kit';
-
-export function load({ params }: Page) {
-	console.log(params.videoId);
-
-	return { videoId: params.videoId };
-}
